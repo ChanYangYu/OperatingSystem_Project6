f84d764d0d9993f5db8c07c04adaec8d339b501a
ChanYangYu <clsrnthd7@gmail.com>
2020-11-23 01:31:56 +0900

    Read, Write Implementation Complete

--- ssufs-ops.c
+++ ssufs-ops.c
@@ -15,6 +15,7 @@ int ssufs_allocFileHandle() {
 int ssufs_create(char *filename){
 	int inode_num;
 	int block_num;
+	char buffer[BLOCKSIZE];
 	struct inode_t new_inode;
 
 	if(open_namei(filename) != -1){
@@ -36,12 +37,17 @@ int ssufs_create(char *filename){
 		ssufs_freeInode(inode_num);
 		return -1;
 	}
+
+	//fill the block with '0'
+	memset(buffer, 0, sizeof(buffer));
+	ssufs_writeDataBlock(block_num, buffer);
 	//read inode
 	ssufs_readInode(inode_num, &new_inode);
 	//set inode
 	new_inode.status = INODE_IN_USE;
 	strcpy(new_inode.name, filename);
 	new_inode.direct_blocks[0] = block_num;
+	new_inode.file_size = 0;
 	//write inode
 	ssufs_writeInode(inode_num, &new_inode);
 
@@ -50,6 +56,8 @@ int ssufs_create(char *filename){
 
 void ssufs_delete(char *filename){
 	int inode_num;
+	char buffer[BLOCKSIZE];
+	struct inode_t info;
 
 	if((inode_num = open_namei(filename)) == -1){
 #if DEBUG
@@ -57,11 +65,18 @@ void ssufs_delete(char *filename){
 #endif
 		return;
 	}
+	ssufs_readInode(inode_num, &info);
+	memset(buffer, 0, sizeof(buffer));
+	//data clear
+	for(int i = 0; i < MAX_FILE_SIZE; i++){
+		if(info.direct_blocks[i] != -1)
+			ssufs_writeDataBlock(info.direct_blocks[i], buffer);
+	}
 	ssufs_freeInode(inode_num);
 }
 
 int ssufs_open(char *filename){
-	int inode_num;
+	int inode_num, file_handle;
 
 	if((inode_num = open_namei(filename)) == -1){
 #if DEBUG
@@ -70,17 +85,18 @@ int ssufs_open(char *filename){
 		return -1;
 	}
 
-	for(int i = 0; i < MAX_OPEN_FILES; i++){
-		if(file_handle_array[i].inode_number = -1){
-			file_handle_array[i].inode_number = inode_num;
-			file_handle_array[i].offset = 0;
-			return i;
-		}
+	file_handle =  ssufs_allocFileHandle();
+	if(file_handle != -1){
+		file_handle_array[file_handle].inode_number = inode_num;
+		file_handle_array[file_handle].offset = 0;
+		return file_handle;
 	}
+	else{
 #if DEBUG
 	fprintf(stderr,"The file has been open to maximum\n");
 #endif
-	return -1;
+		return -1;
+	}
 }
 
 void ssufs_close(int file_handle){
@@ -89,11 +105,102 @@ void ssufs_close(int file_handle){
 }
 
 int ssufs_read(int file_handle, char *buf, int nbytes){
-	/* 4 */
+	int start_block, offset, start_idx, cnt = 0;
+	struct inode_t info; 
+	char buffer[BLOCKSIZE];
+
+	offset = file_handle_array[file_handle].offset;
+	if(ssufs_lseek(file_handle, nbytes) == -1){
+#if DEBUG
+	fprintf(stderr,"The size to read exceeds the file size\n");
+#endif
+		return -1;
+	}
+	ssufs_readInode(file_handle_array[file_handle].inode_number, &info);
+	start_block = offset / BLOCKSIZE;
+	start_idx = offset % BLOCKSIZE;
+	ssufs_readDataBlock(info.direct_blocks[start_block], buffer);
+
+	while(cnt < nbytes){
+		buf[cnt++] = buffer[start_idx++];
+		if(start_idx == BLOCKSIZE && cnt < nbytes){
+			start_block++;
+			ssufs_readDataBlock(info.direct_blocks[start_block], buffer);
+			start_idx = 0;
+		}
+	}
+	buf[cnt] = '\0';
+	return 0;
 }
 
 int ssufs_write(int file_handle, char *buf, int nbytes){
-	/* 5 */
+	int offset, inode, diff, create_num = 0, cnt = 0;
+	int start_block, start_idx, data_index;
+	char buffer[BLOCKSIZE];
+	struct inode_t info;
+
+	offset = file_handle_array[file_handle].offset;
+	if(offset + nbytes > (BLOCKSIZE * MAX_FILE_SIZE)){
+#if DEBUG
+	fprintf(stderr,"The size to write exceeds the file size\n");
+#endif	
+		return -1;
+	}
+	inode = file_handle_array[file_handle].inode_number;
+	ssufs_readInode(inode, &info);
+
+	start_block = offset / BLOCKSIZE;
+	start_idx = offset % BLOCKSIZE;
+	
+	diff = BLOCKSIZE - start_idx;
+	while(diff < nbytes){
+		create_num++;
+		if(info.direct_blocks[start_block + create_num] == -1){
+			if((data_index = ssufs_allocDataBlock()) == -1){
+				for(int i = 1; i < create_num; i++){
+					ssufs_freeDataBlock(info.direct_blocks[start_block + i]);
+					info.direct_blocks[start_block + i] = -1;
+				}
+#if DEBUG
+	fprintf(stderr,"Not enough free block\n");
+#endif			
+				return -1;
+			}
+			else
+				info.direct_blocks[start_block + create_num] = data_index;
+		}
+		diff += BLOCKSIZE;
+	}
+	//fill the new block with '0'
+	memset(buffer, 0, sizeof(buffer));
+	for(int i = 1; i <= create_num; i++)
+		ssufs_writeDataBlock(info.direct_blocks[start_block + i], buffer);
+	
+	ssufs_readDataBlock(info.direct_blocks[start_block], buffer); 
+	//overwrite
+	if(info.file_size > offset){
+		while(offset < info.file_size && cnt < nbytes){
+			buffer[start_idx++] = buf[cnt++];
+			if(start_idx == BLOCKSIZE){
+				ssufs_writeDataBlock(info.direct_blocks[start_block], buffer);
+				start_block++;
+				start_idx = 0;
+			}
+		}
+	}
+	//write
+	while(cnt < nbytes){
+		buffer[start_idx++] = buf[cnt++];
+		if(start_idx == BLOCKSIZE){
+			ssufs_writeDataBlock(info.direct_blocks[start_block], buffer);
+			start_block++;
+			start_idx = 0;
+		}
+		info.file_size++;
+	}
+	ssufs_writeInode(inode, &info);
+	ssufs_lseek(file_handle, nbytes);
+	return 0;
 }
 
 int ssufs_lseek(int file_handle, int nseek){
---
+++ ssufs_test2.c
@@ -0,0 +1,33 @@
+#include "ssufs-ops.h"
+
+int main()
+{
+    char str[] = "!-------32 Bytes of Data-------!!-------32 Bytes of Data-------!";
+	char test1[256], test2[256];
+
+	for(int i = 0; i < 255; i++)
+		test1[i] = 'A';
+    ssufs_formatDisk();
+
+    ssufs_create("f1.txt");
+    int fd1 = ssufs_open("f1.txt");
+
+	ssufs_create("f2.txt");
+	int fd2 = ssufs_open("f2.txt");
+
+    printf("Write Data: %d\n", ssufs_write(fd1, str, BLOCKSIZE));
+	printf("Write Data: %d\n", ssufs_write(fd2, test1, sizeof(test1)));
+    printf("Seek: %d\n", ssufs_lseek(fd2, -100));
+	ssufs_read(fd2, test2, 100);
+	printf("Read Data : %s\n", test2);
+    printf("Seek: %d\n", ssufs_lseek(fd2, -100));
+
+	for(int i = 0; i < 100; i++)
+		test1[i] = 'B';
+	printf("Write Data: %d\n", ssufs_write(fd2, test1, 100));
+    ssufs_dump();
+    ssufs_delete("f1.txt");
+    ssufs_dump();
+	ssufs_delete("f2.txt");
+	ssufs_dump();
+}
